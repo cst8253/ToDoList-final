b53e0d96c3d755c8dbbee24e9dab709704c5ee73
Michael Eisenbraun <eisenbm@algonquincollege.com>
2024-05-31 09:09:44 -0400

    Creates final version

---
+++ Item.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoList;
+
+public class Item (string task)
+{
+  public string Task { get; set; } = task;
+  public bool IsComplete { get; set; } = false;
+
+  public void MarkTask () 
+  {
+    IsComplete = true;
+  }
+}
